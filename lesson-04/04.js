2b89433a7bce33dc5f37e1657505f89aa5bfa03a
Lokiiix <147723544+Lokiiix@users.noreply.github.com>
2025-02-13 02:07:01 +0700

    lesson-upd

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,8 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+  return str.split('').map(c => c + c).join('');
+}
+console.log(doubleEachCharacter('hello'));
+
--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,17 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(galleryObj, artwork, newValue) {
+	galleryObj[artwork] = newValue;
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506');
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali');
+
+console.log(gallery);
+// {
+//   'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
+//   'Starry Night': 'Vincent van Gogh',
+//   'The Scream': 'Edvard Munch',
+//   'The Persistence of Memory': 'Salvador Dali'
+// }
